e1fec7982763e9899a8405238759da0c37b954e4
lavaswimmer <th.hooijer@gmail.com>
2017-10-27 15:11:46 +0200

    Fixed JS to change tamagotchi name

--- www/js/code.js
+++ www/js/code.js
@@ -1,7 +1,8 @@
-geefnaam() {
-  var naam = prompt("Enter your pets name", "Thom");
+function geefNaam() {
+  naam = prompt("Enter your pets name", "Thom");
 
-  if (naam != null) {
-    document.getElementById("naam") = naam;
+  if (naam !== null) {
+    document.getElementById("naam").innerHTML = naam;
+    document.title = naam+"agotchi";
   }
 }
