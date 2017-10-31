6712e6a00a0ee3c844cd56f1dcbe2b5497dc74cf
lavaswimmer <th.hooijer@gmail.com>
2017-10-31 10:47:13 +0100

    set limit to food/enjoyment/sleep

--- platforms/android/assets/www/js/code.js
+++ platforms/android/assets/www/js/code.js
@@ -10,9 +10,9 @@ function geefNaam() {
 
 // consuming the food/play/sleep
 
-food = 100;
-enjoy = 100;
-sleep = 100;
+food = 110;
+enjoy = 110;
+sleep = 110;
 
 setInterval(
   window.onload = function consume() {
@@ -24,20 +24,38 @@ setInterval(
     document.getElementById("food-p").value = food;
     document.getElementById("enjoyment-p").value = enjoy;
     document.getElementById("sleep-p").value = sleep;
+    console.log("food: "+food);
+    console.log("enjoyment: "+enjoy);
+    console.log("sleep: "+sleep);
+    console.log("");
   }
 ,5000);
 
+// functions to add more food/enjoyment/sleep
 function feed() {
-  food +=15;
-  document.getElementById("food-p").value = food;
+  if (food < 100) {
+    food +=15;
+    document.getElementById("food-p").value = food;
+  }else {
+    food +=0;
+  }
 }
 
 function enjoying() {
-  enjoy +=15;
-  document.getElementById("enjoyment-p").value = enjoy;
+  if (enjoy < 100) {
+    enjoy +=15;
+    document.getElementById("enjoyment-p").value = enjoy;
+  }else {
+    enjoy +=0;
+  }
+
 }
 
 function sleeping() {
-  sleep +=15;
-  document.getElementById("sleep-p").value = sleep;
+  if (sleep < 100) {
+    sleep +=15;
+    document.getElementById("sleep-p").value = sleep;
+  }else {
+    sleep +=0;
+  }
 }
