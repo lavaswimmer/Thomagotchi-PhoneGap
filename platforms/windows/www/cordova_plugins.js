92094afc9fa937a024fcb3c631b96125dddcc073
Unknown <th.hooijer@gmail.com>
2017-11-03 13:56:30 +0100

    added windows support

---
+++ platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,24 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-dialogs.notification",
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "cordova-plugin-dialogs.NotificationProxy",
+        "file": "plugins/cordova-plugin-dialogs/src/windows/NotificationProxy.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-dialogs": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
---
+++ platforms/windows/www/js/code.js
@@ -0,0 +1,97 @@
+﻿// function to change the tamagotchi name
+var naam = "Thom";
+
+function geefNaam() {
+  naam = prompt("Enter your pets name", "Thom");
+
+  if (naam !== null) {
+    document.getElementById("naam").innerHTML = naam;
+    document.title = naam+"agotchi";
+  }
+}
+
+// consuming the food/play/sleep
+
+food = 110;
+enjoy = 110;
+sleep = 110;
+
+setInterval(
+  window.onload = function consume() {
+    if (food > 0) {
+      food -=10;
+      document.getElementById("food-p").value = food;
+    }else {
+      food -=0;
+      document.getElementById("food-p").value = food;
+    }
+
+    if (enjoy > 0) {
+      enjoy -=10;
+      document.getElementById("enjoyment-p").value = enjoy;
+    }else {
+      enjoy -=0;
+      document.getElementById("enjoyment-p").value = enjoy;
+    }
+
+    if (sleep > 0) {
+      sleep -=10;
+      document.getElementById("sleep-p").value = sleep;
+    }else {
+      sleep -=0;
+      document.getElementById("sleep-p").value = sleep;
+    }
+
+    console.log("food: "+food);
+    console.log("enjoyment: "+enjoy);
+    console.log("sleep: "+sleep);
+    console.log("");
+  }
+,1000);
+
+// functions to add more food/enjoyment/sleep
+function feed() {
+  if (food < 100) {
+    food +=15;
+    document.getElementById("food-p").value = food;
+  }else {
+    food +=0;
+  }
+}
+
+function enjoying() {
+  if (enjoy < 100) {
+    enjoy +=15;
+    document.getElementById("enjoyment-p").value = enjoy;
+  }else {
+    enjoy +=0;
+  }
+}
+
+function sleeping() {
+  if (sleep < 100) {
+    sleep +=15;
+    document.getElementById("sleep-p").value = sleep;
+  }else {
+    sleep +=0;
+  }
+}
+
+// Dead
+
+console.log(food);
+
+setInterval(
+  function dead() {
+    if (food <= 0) {
+      alert(naam+" is dead");
+      location.reload();
+    }else if (enjoy <= 0) {
+      alert(naam+" is dead");
+      location.reload();
+    }else if (sleep <= 0) {
+      alert(naam+" is dead");
+      location.reload();
+    }
+  }
+,100);
